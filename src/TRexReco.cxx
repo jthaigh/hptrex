d17f5cfc5d87eac5767c0626708079313223699e
Martin Haigh <phsmaj@soulor.epp.warwick.ac.uk>
2016-10-12 13:47:46 +0100

    Remove a load of irrelevant stuff from high-level part of TREx.

--- src/TRexReco.cxx
+++ src/TRexReco.cxx
@@ -5,14 +5,7 @@
 #include <vector>
 #include <memory>
 
-#include <TND280Event.hxx>
-#include <TGeomInfo.hxx>
-#include <TOARuntimeParameters.hxx>
-#include <THandle.hxx>
-#include <TAlgorithmResult.hxx>
-
 #include "TRexReco.hxx"
-#include "TTPCCalibration.hxx"
 #include "TTPCRecPackUtils.hxx"
 #include "TTPCUtils.hxx"
 
@@ -20,36 +13,12 @@
 /*******************
  ** Constructor   **
  ******************/
-TRexReco::TRexReco(): ND::TAlgorithm("TRexReco") {
+TRexReco::TRexReco(){
 
-  ND::TND280Log::SetLogLevel("TREx",ND::TND280Log::QuietLevel);
-
-  TTPCRecPackUtils::InitRecPackManager();
-
-  // Output
-  fSavePatRecResults = ND::TOARuntimeParameters::Get().GetParameterI("trexRecon.SavePatternRecognitionResults");
-  fSaveUnmergedResults = ND::TOARuntimeParameters::Get().GetParameterI("trexRecon.SaveGasInteractionOutput");
-  fSaveTRExContainers = ND::TOARuntimeParameters::Get().GetParameterI("trexRecon.SaveTRExContainers");
-
-  // skip complicated shower events
-  fMaxNbWaveforms = ND::TOARuntimeParameters::Get().GetParameterI("trexRecon.Reco.MaxNbWaveforms");
-
-  // Waveform treatment
-  fWaveformAlgo = new TTPCWaveformDigest();
-
-  // Pattern reco
-  fPatRecAlgo = new ND::TTPCTRExPatAlgorithm();
-
-  // Seeding
   fSeedingAlgo = new ND::TTPCSeeding();
 
   // T0 fit 
   fT0Algo = new ND::TTPCT0Finder();
-
-  // Merge across the MM horizontal gap
-  fMMHoriGapMMergeAlgo = new ND::TTPCMMHoriGapMerge();
-  // Merge across the MM vertical gap
-  fMMVertGapMMergeAlgo = new ND::TTPCMMVertGapMerge();
 
   // Tracking: Likelihood fit and others like momentum by range
   fTrackingAlgo = new ND::TTPCTracking();
@@ -63,11 +32,6 @@ TRexReco::TRexReco(): ND::TAlgorithm("TRexReco") {
   // Merge all Likelihood matches
   fLikelihoodMergeAlgo = new ND::TTPCLikelihoodMerge();
 
-  // Merge across the MM vertical gap
-  fCathCrosserMergeAlgo = new ND::TTPCCathCrosserMerge(fT0Algo);
-
-  //ftimer.start(NULL);
-
 }
 
 
@@ -76,18 +40,6 @@ TRexReco::TRexReco(): ND::TAlgorithm("TRexReco") {
  ******************/
 TRexReco::~TRexReco() {
 
-  delete fWaveformAlgo;
-  delete fPatRecAlgo;
-  delete fSeedingAlgo;
-
-  delete fT0Algo;
-  delete fMMHoriGapMMergeAlgo;
-  delete fMMVertGapMMergeAlgo;
-  delete fTrackingAlgo;
-  delete fLklhdMatchAlgo;
-  delete fPIDAlgo;
-  delete fLikelihoodMergeAlgo;
-  delete fCathCrosserMergeAlgo;
 
 }
 
@@ -99,48 +51,10 @@ TRexReco::~TRexReco() {
  *******************************************************************/
 ND::THandle<ND::TAlgorithmResult> TRexReco::Process(const ND::TAlgorithmResult& in) {
 
-  ND::THandle<ND::TAlgorithmResult> result( CreateResult() );
-  ND::THitSelection* Used = new ND::THitSelection("TRExused");
-  ND::THitSelection* Unused = new ND::THitSelection("TRExunused");
-
-  unsigned int stepCount = 1;
-
-  //ftimer.restart(NULL);
-  ND::TND280Event& event = GetEvent();
-
-
-  //___________ Read the calibration tables ___________
-  ND::tpcCalibration().ReadCalibration(event);
-
-  
-  //___________ Hit preparation ___________
-  if ( ND::tpcDebug().GeneralSteps(DB_INFO))
-    std::cout<<" ============= TRExReco step "<<stepCount++<<": Hit preparation"<<std::endl;
-  ND::THandle<ND::THitSelection> tpc(new ND::THitSelection("tpc"));
-  fWaveformAlgo->Process(event, tpc);
-
-  if( tpc->size() > fMaxNbWaveforms){
-    std::cout<< "TREX WARNING: too many waveforms in the TPCs ("<<tpc->size()<<" waveforms). Give up the TPC reconstruction entirely"<<std::endl;
-    return result;
-  }
-  
-  //___________ Pattern recognition ___________
-  // These patterns are for each MM column of each read out plane independently
-  // Save the pattern recognition results if we want to.
-  if ( ND::tpcDebug().GeneralSteps(DB_INFO))
-    std::cout<<" ============= TRExReco step "<<stepCount++<<": Pattern recognition"<<std::endl;
-  fPatRecAlgo->Process(tpc, Used, Unused);
-  result->AddHitSelection(Used);
-  result->AddHitSelection(Unused);
-  ND::TReconObjectContainer *PartColumnPatterns = new ND::TReconObjectContainer("TRExPatternReco");
   fPatRecAlgo->GetPatterns(PartColumnPatterns);
 
   // Define a T0 that puts all the hits in the drift volume if possible.
   fT0Algo->CalculateDefaultT0(PartColumnPatterns);
-
-  if ( ND::tpcDebug().GeneralSteps(DB_VERBOSE)) {
-    PrintoutObjects(PartColumnPatterns);
-  }
 
   // The pattern recognition didn't find anything. We can stop here.
   if ( ! PartColumnPatterns->size())
@@ -163,36 +77,6 @@ ND::THandle<ND::TAlgorithmResult> TRexReco::Process(const ND::TAlgorithmResult& 
   for (ND::TReconObjectContainer::iterator pattern = PartColumnPatterns->begin(); pattern != PartColumnPatterns->end(); pattern++) {
       fSeedingAlgo->Process(*pattern);
   }
-
-
-  //___________ Merging across MM horizontal gap ___________
-  // Some of the patterns are broken at the horizontal gap.
-  // If a pattern is not matched to another one, then it is just copied to ColumnPatterns.
-  // If two pattern are matched, only one is saved to the output with the added content of the second pattern.
-  // The second pattern can be deleted ... we have to be careful in terms of memory management.
-  if ( ND::tpcDebug().GeneralSteps(DB_INFO))
-    std::cout<<" ============= TRExReco step "<<stepCount++<<": MM horizontal gap merging"<<std::endl;
-  ND::TReconObjectContainer *ColumnPatterns = new ND::TReconObjectContainer("TRExPatternReco");
-  fMMHoriGapMMergeAlgo->Process(PartColumnPatterns, ColumnPatterns);
-
-  if ( ND::tpcDebug().GeneralSteps(DB_VERBOSE)) {
-    PrintoutObjects(ColumnPatterns);
-  }
-
-  //___________ Merging across MM vertical gap ___________
-  // Now our patterns are for each read out plane.
-  // If a pattern is not matched to another one, then it is just copied to ReadOutPatterns.
-  // If two pattern are matched, only one is saved to the output with the added content of the second pattern.
-  // The second pattern can be deleted ... we have to be careful in terms of memory management.
-  if ( ND::tpcDebug().GeneralSteps(DB_INFO))
-    std::cout<<" ============= TRExReco step "<<stepCount++<<": MM vertical gap merging"<<std::endl;
-  ND::TReconObjectContainer *ReadOutPatterns = new ND::TReconObjectContainer("TRExUnmergedReco");
-  fMMVertGapMMergeAlgo->Process(ColumnPatterns, ReadOutPatterns);
-
-  if ( ND::tpcDebug().GeneralSteps(DB_VERBOSE)) {
-    PrintoutObjects(ReadOutPatterns);
-  }
-
   // We don't want to prepare the hits of the other detectors for the T0 search
   // for every single pattern so let's do it here.
   fT0Algo->PrepareScintHits(event);
@@ -242,12 +126,6 @@ ND::THandle<ND::TAlgorithmResult> TRexReco::Process(const ND::TAlgorithmResult& 
   // gas and standard output objects.
   ND::tpcCalibration().OffsetIds();
 
-  //___________ Merging tracks crossing the TPC ___________
-  if ( ND::tpcDebug().GeneralSteps(DB_INFO))
-  std::cout<<" ============= TRExReco step "<<stepCount++<<": Merging through going and broken tracks"<<std::endl;
-  ND::TReconObjectContainer *ReadOutMergedPatterns = new ND::TReconObjectContainer("TRExLklhdMergedReco");
-  fLikelihoodMergeAlgo->Process(ReadOutPatterns, ReadOutMergedPatterns);
-
   // We can here loop over all the patterns again
   for (ND::TReconObjectContainer::iterator pattit = ReadOutMergedPatterns->begin(); pattit != ReadOutMergedPatterns->end(); pattit++) {
     // The output will be like what is done in tpcRecon in terms of recombining tracks broken by delta rays.
@@ -270,20 +148,6 @@ ND::THandle<ND::TAlgorithmResult> TRexReco::Process(const ND::TAlgorithmResult& 
     
   }
   stepCount+=3;
-
-  if ( ND::tpcDebug().GeneralSteps(DB_VERBOSE)) {
-    PrintoutObjects(ReadOutMergedPatterns);
-  }
-
-  //___________ Merging across cathode ___________
-  if ( ND::tpcDebug().GeneralSteps(DB_INFO))
-  std::cout<<" ============= TRExReco step "<<stepCount++<<": Merging cathode crossers"<<std::endl;
-  ND::TReconObjectContainer *FullTPCPatterns = new ND::TReconObjectContainer("TRExAllMergedReco");
-  fCathCrosserMergeAlgo->Process(ReadOutMergedPatterns, FullTPCPatterns);
-
-  if ( ND::tpcDebug().GeneralSteps(DB_VERBOSE)) {
-    PrintoutObjects(FullTPCPatterns);
-  }
 
   //___________ Save the tpcRecon-like output ___________
   // Here we save the output that will be used in the Prod7 globalRecon.
@@ -322,64 +186,3 @@ ND::THandle<ND::TAlgorithmResult> TRexReco::Process(const ND::TAlgorithmResult& 
 }
 
 
-// *******************************************************************
-void TRexReco::StoreContainerInOutput( ND::THandle<ND::TAlgorithmResult> algoResult, ND::TReconObjectContainer *container, std::string oaEventName){
-  if (fSaveTRExContainers){
-    algoResult->AddResultsContainer(container);
-  }
-  else {
-    // Convert TTPCPatterns to TReconVertex if there is one or more Junction.
-    // Convert to TReconTrack if there is no Junction.
-    // This is done automatically.
-    ND::TReconObjectContainer *OutputPatterns = new ND::TReconObjectContainer(oaEventName.c_str());
-    for (ND::TReconObjectContainer::iterator pattern = container->begin(); pattern != container->end(); pattern++) {
-      ND::THandle<ND::TTPCPattern> Pattern = (*pattern);
-      ND::THandle<ND::TReconBase> oaEvtPattern = Pattern->ConvertToOAEvent();
-      if ( ND::tpcDebug().GeneralSteps(DB_VVERBOSE)) {
-        std::cout<<" ============= Constituent structure"<<std::endl;
-        TTPCUtils::PrintConstituentMap(oaEvtPattern,0);
-      }
-      OutputPatterns->push_back(oaEvtPattern);
-    }
-    algoResult->AddResultsContainer(OutputPatterns);
-  }
-
-}
-
-
-// *******************************************************************
-void TRexReco::PrintoutObjects(ND::TReconObjectContainer *inPatterns){
-  for (ND::TReconObjectContainer::iterator colPatIt = inPatterns->begin(); colPatIt < inPatterns->end(); colPatIt++)
-  {
-    ND::THandle<ND::TTPCPattern> pattern = *colPatIt;
-    if (!pattern){
-      std::cout << "   ============= TRExReco SAFETY WARNING: Object from pattern recognition is NOT a TTPCPattern !" << std::endl;
-      continue;
-    }
-
-    ND::THandle<ND::TReconObjectContainer> constituents = pattern->GetConstituents();
-    int nconst = constituents->size();
-    if (!nconst) {
-      std::cout << " ============= TRExReco WARNING: TTPCPattern has 0 constituents !" << std::endl;
-      continue;
-    }
-    std::cout << " =============> pattern, id "<<pattern->GetId() << std::endl;
-
-    for (ND::TReconObjectContainer::iterator constIt = constituents->begin(); constIt != constituents->end(); constIt++)
-    {
-      ND::THandle<ND::TTPCPath> path = *constIt;
-      ND::THandle<ND::TTPCJunction> junction = *constIt;
-      if (path){
-        std::cout << "  |-> path, id "<<path->GetId() << std::endl;
-      } else if (junction) {
-        std::cout << "  |-> junction, id "<<junction->GetId() << std::endl;
-        for (ND::TReconObjectContainer::iterator pathIt = junction->GetConstituents()->begin(); pathIt != junction->GetConstituents()->end(); pathIt++){
-          path = *pathIt;
-          std::cout << "  |   |-> path, id "<<path->GetId() << std::endl;
-        }
-      } else {
-        std::cout << " ============= TRExReco WARNING: A constituent of the pattern is neither a TTPCPath, nor a TTPCJunction !" << std::endl;
-      }
-    }
-  }
-}
--- src/TRexReco.hxx
+++ src/TRexReco.hxx
@@ -1,46 +1,28 @@
 #ifndef TRexReco_hxx_seen
 #define TRexReco_hxx_seen
 
-#include <EoaCore.hxx>
-#include <TAlgorithm.hxx>
-#include <THandle.hxx>
-//#include "timer.h"   --> exists in tpcRecon (use a common used one instead!!)
-//#include "TRecPackManager.hxx"
-
-#include <THitSelection.hxx>
-
-#include "TTPCWaveformDigest.hxx"
 #include "patternRecognition/TTPCTRExPatAlgorithm.hxx"
 
 #include "TTPCSeeding.hxx"
-#include "TTPCMMHoriGapMerge.hxx"
-#include "TTPCMMVertGapMerge.hxx"
 #include "TTPCT0Finder.hxx"
 #include "TTPCTracking.hxx"
 #include "TTPCLikelihoodMatch.hxx"
 #include "TTPCPID.hxx"
 #include "TTPCLikelihoodMerge.hxx"
-#include "TTPCCathCrosserMerge.hxx"
 
 // Basic algorithm class for the TPC reconstruction. It takes cares of the input-output data handling and calling the different algorithms in the right order.
 //
 
-class TRexReco: public ND::TAlgorithm {
+class TRexReco{
 public:
   
   // Default constructor
   TRexReco();
   // Default destructor.
   virtual ~TRexReco();
-
-  // Hit preparation. 
-  ND::THandle< ND::TAlgorithmResult> HitPreparation(void);
   
   // Default algorithm process method. This is the core of the tpcRecon algorithm.
   ND::THandle<ND::TAlgorithmResult> Process(const ND::TAlgorithmResult&);
-  
-  void StoreContainerInOutput( ND::THandle<ND::TAlgorithmResult> algoResult, ND::TReconObjectContainer *container, std::string oaEventName);
-  
   
   // Set the diffusion Sigma for the likelihood fit.
   // Well, the fitter should do that. This is the property of the fit
@@ -53,30 +35,9 @@ public:
     }*/
   
 private: 
-  void PrintoutObjects(ND::TReconObjectContainer *inPatterns);
-
-  //timer ftimer; 
   
-  // Output
-  bool fSavePatRecResults;
-  bool fSaveUnmergedResults;
-  bool fSaveTRExContainers;
-
-  int fMaxNbWaveforms;
-  
-  // Waveform treatment
-  TTPCWaveformDigest* fWaveformAlgo;
-
-  // Pattern recognition
-  ND::TTPCTRExPatAlgorithm* fPatRecAlgo;
-
   /// Seeding
   ND::TTPCSeeding* fSeedingAlgo;
-
-  /// Merge across the horizontal MM gap
-  ND::TTPCMMHoriGapMerge* fMMHoriGapMMergeAlgo;
-  /// Merge across the vertical MM gap
-  ND::TTPCMMVertGapMerge* fMMVertGapMMergeAlgo;
 
   /// T0 finder
   ND::TTPCT0Finder* fT0Algo;
@@ -94,10 +55,6 @@ private:
 
   /// Merge paths crossing a junction or from a broken track
   ND::TTPCLikelihoodMerge* fLikelihoodMergeAlgo;
-
-  /// Merge paths crossing the cathode
-  ND::TTPCCathCrosserMerge* fCathCrosserMergeAlgo;
-
 
 };
 
