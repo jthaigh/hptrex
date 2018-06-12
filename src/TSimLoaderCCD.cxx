6928be8a0176b802f911d1dd4364e7a8eb4982e5
svalder <sammyvalder@gmail.com>
2018-06-12 15:47:16 +0100

    Added changes to voxelization, PIDefficiency and TSimLoaderCCD

---
+++ src/TSimLoaderCCD.cxx
@@ -0,0 +1,163 @@
+#include "TSimLoaderCCD.hxx"
+
+
+trex::TSimLoader::TSimLoader(std::string inputFile){
+  
+  fFile=new TFile(inputFile.c_str(), "UPDATE");
+  
+
+  fVoxelsTree=(TTree*)fFile->Get("VoxelsTree");
+
+  fVoxelBranch=0;
+  fVoxelsTree->SetBranchAddress("voxels", &fVoxelBranch);
+
+  Detector = new TH3D("Detector", "Detector", 513, -710., 710., 513, -710., 710., 1, 0, 1);
+  Detector->SetDirectory(0);
+}
+
+
+void trex::TSimLoader::LoadEvent(unsigned int i){
+
+  Int_t EventNumber=0;
+  fVoxelsTree->SetBranchAddress("EventNumber", &EventNumber);
+
+  Double_t Momentum=0;
+  fVoxelsTree->SetBranchAddress("Momentum", &Momentum);
+
+  TVector3* TrueXi=0;
+  fVoxelsTree->SetBranchAddress("Xi", &TrueXi);
+
+  TVector3* TrueXf=0;
+  fVoxelsTree->SetBranchAddress("Xf", &TrueXf);
+  
+  Int_t PDG=0;
+<<<<<<< HEAD
+  fVoxelsTree->SetBranchAddress("pdg", &PDG);
+=======
+  fVoxelsTree->SetBranchAddress("PDG", &PDG);
+>>>>>>> 6f00b7874a1b0801b88832bb262700781aae568e
+  
+  Int_t TrackID = 0;
+  fVoxelsTree->SetBranchAddress("TrackID", &TrackID);
+  
+  Int_t ParentID = 0;
+  fVoxelsTree->SetBranchAddress("ParentID", &ParentID);
+  
+ // Int_t ProOrPi = 0;
+ // fVoxelsTree->SetBranchAddress("ProOrPi", &ProOrPi);
+
+
+  std::cout << "LOADING SPILL # " << i << std::endl;
+
+  fVoxelsTree->GetEntry(i);
+
+  std::cout << "Processing Image # " << EventNumber << std::endl;
+  
+   
+  //Clean up previous event but only if we are actually processing a new image - if not just add another single track to the event.
+  if(EventNumber != fimageNumber){
+
+    //Delete hits from last event
+    for(std::vector<trex::TTPCHitPad*>::iterator hitPadIter=fHits.begin();hitPadIter!=fHits.end();++hitPadIter){
+      delete *hitPadIter;
+    }
+    
+    //Delete voxels from last event
+    for(std::vector<voxel*>::iterator voxelIter=fVoxels.begin(); voxelIter!=fVoxels.end();++voxelIter){
+      delete *voxelIter;
+    }
+    
+    
+    //Delete TrueHits from last event
+    for(std::vector<TTrueHit*>::iterator TrueHitIter=fTrueHits.begin(); TrueHitIter!=fTrueHits.end();++TrueHitIter){
+      delete *TrueHitIter;
+    }
+    
+    //Delete TrueTracks from last event
+    for(std::vector<trex::TTrueTrack*>::iterator truetrackIter=fTrueTracks.begin(); truetrackIter!=fTrueTracks.end(); ++truetrackIter){
+      delete *truetrackIter;
+    }
+
+    
+    fHits.clear();
+    fVoxels.clear();
+    fTrueHits.clear();
+    fTrueTracks.clear();
+
+    fimageNumber = EventNumber;
+    
+  }
+
+  fTrueTracks.push_back(new TTrueTrack());
+ 
+  Int_t TrackNumber = fTrueTracks.size();
+  std::cout << "This is Track number " << TrackNumber << " of Image number " << EventNumber << std::endl;
+
+  fTrueMultiplicity = TrackNumber;
+  
+  fTrueTracks.back()->SetEntries(PDG,TrackNumber,TrackID,/* ProOrPi,*/ ParentID, *TrueXi, *TrueXf, Momentum);
+
+  Int_t nVoxels = fVoxelBranch->GetNbins(); 
+  
+  std::cout << "There are " << nVoxels << " Voxels in this Event" << std::endl;
+
+  if(TrackID==1){
+    fTrueTracks.back()->SetNumberOfHits(nVoxels);}
+  else{std::cout << "THIS IS A TRACK WITH HIGHER TRACK ID!!!" << std::endl;}
+ 
+  for(int linInd=0; linInd<nVoxels; ++linInd){
+          
+    fVoxels.push_back(new voxel);
+    voxel * voxelPtr = fVoxels.back();
+
+    //Extracting information from the THnSparseF
+   
+    //Hard-coded Histogram dimensions (this is not good)
+    Int_t coords[3];
+    Double_t position[3];
+        
+    //PRD original resolution
+    //double res = 2.34;
+    //Int_t bins[3] = {513, 513, 1};
+    //Double_t maxs[3] = { 600.21,  600.21, 0.};
+    //Double_t mins[3] = {-600.21, -600.21, 1.};
+
+    (*voxelPtr).Edep = fVoxelBranch->GetBinContent(linInd, coords);
+    (*voxelPtr).time = 0; //setting time to 0 for now until we have a T0 from other subdetectors
+
+    //Translate coordinates into positions
+    //This requires a bit more thought. For now position = coordinates works. 
+    for(int dim=0; dim<3; ++dim){
+      //position[dim]=(mins[dim]) + res*coords[dim]; //real position (not good for cell-ID map right now)
+      position[dim]=coords[dim];
+    }
+
+    //Swapping coordinates here because t2k beam direction is z and drift direction is x, whereas PRD beam direction is y and drift direction is z. 
+    (*voxelPtr).x_pos = position[2];
+    (*voxelPtr).y_pos = position[0];
+    (*voxelPtr).z_pos = position[1];
+
+    //std::cout << "Have found Voxel at position: " << coords[0] << " : " << coords[1] << " : " << coords[2] << std::endl;
+    
+    TLorentzVector pos4((*voxelPtr).x_pos, (*voxelPtr).y_pos, (*voxelPtr).z_pos, (*voxelPtr).time);
+
+    trex::TTPCHitPad* hitPadPtr=new TTPCHitPad((*voxelPtr).Edep,pos4);
+    hitPadPtr->SetTrueTrack(fTrueTracks.back());
+
+    fHits.push_back(hitPadPtr);
+
+    Detector->Fill(pos4.X(), pos4.Y(), pos4.Z());
+
+  }
+
+  //std::cout << "These Hits belong to a Track with TrackID: " << fTrueTracks.back()->GetTrackID() << std::endl;
+ 
+}
+
+
+unsigned int trex::TSimLoader::GetNEvents(){
+  return fVoxelsTree->GetEntries();
+}
+
+
+
---
+++ src/macros/PIDefficiency.cc
@@ -0,0 +1,377 @@
+#include "TFile.h" 
+#include "TTree.h"
+#include "../TTRExPattern.hxx"
+#include "../TTPCHitPad.hxx" 
+#include "TH1.h"  
+#include "TH2.h"
+#include "TCanvas.h"
+#include "TChain.h"
+#include "TFileInfo.h"
+#include "math.h"
+#include "TStyle.h"
+
+
+void PIDmacro(){
+
+  TChain * ReconTree = new TChain("TPCRecon");
+<<<<<<< HEAD
+   
+  
+
+/*  
+  ReconTree->Add("~/TREx/hptrex/src/TRExRecon_plot_pBiasedElasticProton7.7_vs_piUnbiasedPion19.3_voxelsIdeal_234e-5m_234e-5m.root");
+  ReconTree->Add("~/TREx/hptrex/src/TRExRecon_plot_pBiasedInelasticProton7.7_vs_piUnbiasedPion19.3_voxelsIdeal_234e-5m_234e-5m.root");
+  ReconTree->Add("~/TREx/hptrex/src/TRExRecon_plot_pUnbiasedProton7.7_vs_piUnbiasedPion19.3_voxelsIdeal_234e-5m_234e-5m.root");
+*/
+
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_0.0_unPiAvg_0.0_ElPro_0.0_InePro_1.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_0.0_unPiAvg_0.0_ElPro_1.0_InePro_0.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_0.0_unPiAvg_1.0_ElPro_0.0_InePro_0.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_1.0_unPiAvg_0.0_ElPro_0.0_InePro_0.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_3.0_unPiAvg_3.0_ElPro_0.0_InePro_0.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_5.7_unPiAvg_21.7_ElPro_1.0_InePro_1.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_6.7_unPiAvg_21.7_ElPro_0.0_InePro_1.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  //ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_6.7_unPiAvg_21.7_ElPro_1.0_InePro_0.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+  ReconTree->Add("~/TREx/hptrex/src/Data/tobyData/MergedFiles280318/Processed/277_554/TRExRecon_unProAvg_7.7_unPiAvg_21.7_ElPro_0.0_InePro_0.0_merge_voxelsIdeal_277e-5m_554e-5m.root");
+=======
+  //ReconTree->Add("../TRExRecon_plot_p5_vs_pi10_10_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("../TRExRecon_plot_p5_vs_pi10_1_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_10_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_1_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_2_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_3_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_4_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_5_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_6_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_7_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_8_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/May16MergeTruth/highM_Processed/TRExRecon_plot_p5_vs_pi10_9_voxelsIdeal_234e-5m_234e-5m.root");
+
+  ReconTree->Add("~/DUNE/data/testBeam/August_17_merged/TRExRecon_plot_p6.6_vs_pi17.0_1_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/August_17_merged/TRExRecon_plot_p6.6_vs_pi17.0_2_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/August_17_merged/TRExRecon_plot_p6.6_vs_pi17.0_3_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/August_17_merged/TRExRecon_plot_p6.6_vs_pi17.0_4_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/August_17_merged/TRExRecon_plot_p6.6_vs_pi17.0_5_voxelsIdeal_234e-5m_234e-5m.root");
+  //ReconTree->Add("~/DUNE/data/testBeam/August_17_merged/TRExRecon_plot_p6.6_vs_pi17.0_6_voxelsIdeal_234e-5m_234e-5m.root");
+>>>>>>> 6f00b7874a1b0801b88832bb262700781aae568e
+
+  std::cout << "We are reaching this 1" << std::endl;
+
+  //declare histograms
+  TH1D dEdx("dEdx", "dEdx", 50, 0, 0.00002);
+  TH1D TrackLength("TrackLength", "TrackLength", 300, 0, 1200);
+  TH1D ChargeSum("ChargeSum", "ChargeSum", 200, 0, 0.024);
+
+  TH1D Cleanliness("Cleanliness", "Cleanliness", 100, 0, 1);
+  
+  TH1D* CleanlinessVsM[21];
+  TH1D* CompletenessVsM[21];
+  TH2D* TrackEfficiencyScan[21];
+
+  int compbins=10;
+  int cleanbins=10;
+  double step=0.05;
+
+
+  for(int i=0; i<21; ++i){    
+    char nameClean[20];
+    char nameComp[20];
+    sprintf(nameClean,"Cleanliness_Mult%d", i);
+    sprintf(nameComp, "Completeness_Mult%d", i);
+    CleanlinessVsM[i] = new TH1D(nameClean , "", 100, 0, 1);   
+    CompletenessVsM[i] = new TH1D(nameComp, "", 100, 0, 1);
+    
+    char nameScan[50];
+    sprintf(nameScan, "CleanlinessVsCompletenessCuts_Mult%d", i);
+    TrackEfficiencyScan[i]=new TH2D(nameScan,"", cleanbins, 0.5, 1.00, compbins, 0.5, 1.00);  
+  }
+  
+  TH1D Completeness("Completeness", "Completeness", 100, 0, 1);
+
+  TH1D MeanCleanliness("MeanCleanliness", "MeanCleanliness", 20, 0, 20);
+  TH1D MeanCompleteness("MeanCompleteness", "MeanCompleteness", 20, 0, 20);
+  
+  TH1D TrackEfficiency("TrackEfficiency", "TrackEfficiency", 20, 0, 20);
+  TH1D TrackPurity("TrackPurity", "TrackPurity", 20, 0, 20);
+
+  TH1D ProtonEfficiency("ProtonEfficiency", "ProtonEfficiency",20, 0, 20); 
+  TH1D ProtonPurity("ProtonPurity", "ProtonPurity", 20,0,20);
+
+  TH2D Multiplicities("Multiplicities", "Multiplicities", 20, 0, 20, 20, 0, 20); 
+
+  TH1D PDGs("PDG", "PDG", 3, 0, 3);
+  TH1D GoodPDGs("GoodPDGs", "GoodPDGs", 3, 0, 3);
+  TH1D BadPDGs("BadPDGs", "BadPDGs", 3, 0, 3);
+
+  int entries = ReconTree->GetEntries();
+  
+  trex::WritableEvent * event=0;
+
+  double pion_veto = 3.5e-6;
+  double SingleCleanCut = 0.0;
+  double SingleCompCut = 0.5;
+
+  ReconTree->SetBranchAddress("event", &event);
+   
+  //loop over events
+  
+  int goodProtons[21]={};
+  int badProtons[21]={};
+  int missedProtons[21]={};
+  
+  int totalNumberOfPaths[21]={};
+  int totalNumberOfTrueTracks[21]={};
+  int totalNumberOfGoodTracks[21][10][10]={};
+
+  int totalTracksPassingSingleCut[21]={};
+  
+  for(int i=0; i<entries; ++i) {
+    
+    ReconTree->GetEntry(i);
+
+   
+    int recoMulti = event->RecoMultiplicity;
+    int trueMulti = event->TrueMultiplicity;
+    
+    //std::cout << "The TRUE MULTIPLICITY was: " << trueMulti << std::endl;
+    //std::cout << "The RECO MULTIPLICITY was: " << recoMulti << std::endl;
+    
+    if (trueMulti >20){
+      continue;
+    }
+
+    Multiplicities.Fill(trueMulti, recoMulti);
+    totalNumberOfTrueTracks[trueMulti]+=trueMulti;
+
+
+    //Loop over patterns
+    std::vector<trex::WritablePattern> pats = event->patterns;
+    for(int j=0; j<pats.size(); ++j){
+      
+
+      std::vector<double> dEdx_vec = pats[j].dEdx;
+      std::vector<double> TrackLength_vec = pats[j].TrackLength;
+      std::vector<double> ChargeSum_vec = pats[j].ChargeSum;
+      //declare more vectors
+      std::vector<double> Cleanliness_vec = pats[j].TrackCleanliness;
+      std::vector<double> Completeness_vec = pats[j].TrackCompleteness;
+      std::vector<int> PDG_vec = pats[j].PDG;
+      std::vector<int> PID_vec = pats[j].PID;
+      std::vector<int> PDG_vec = pats[j].PDG;
+
+      //Loop over paths
+      for(int k=0; k<dEdx_vec.size(); ++k){
+	
+	totalNumberOfPaths[trueMulti]+=1;
+	
+	dEdx.Fill(dEdx_vec[k]);
+	TrackLength.Fill(TrackLength_vec[k]);
+	ChargeSum.Fill(ChargeSum_vec[k]);
+	Cleanliness.Fill(Cleanliness_vec[k]);
+	Completeness.Fill(Completeness_vec[k]);
+	
+
+	for(int cleanbin=0; cleanbin<cleanbins; ++cleanbin){
+	  
+	  for(int compbin=0; compbin<compbins; ++compbin){
+	    
+	    double compcut = 0.5+compbin*step;
+	    double cleancut = 0.5+cleanbin*step;
+	    
+	    if (Cleanliness_vec[k] > cleancut && Completeness_vec[k] > compcut){
+	      totalNumberOfGoodTracks[trueMulti][cleanbin][compbin]+=1;
+	    }
+	  }
+	}
+	
+	CleanlinessVsM[trueMulti]->Fill(Cleanliness_vec[k]);
+	CompletenessVsM[trueMulti]->Fill(Completeness_vec[k]);
+
+	std::cout << "PDG_vec[" << k << "] is = " << PDG_vec[k] << std::endl;
+	//apply single cut here
+	if(Cleanliness_vec[k] > SingleCleanCut && Completeness_vec[k] > SingleCompCut){
+
+<<<<<<< HEAD
+	  //CleanlinessVsM[trueMulti]->Fill(Cleanliness_vec[k]);
+	  //CompletenessVsM[trueMulti]->Fill(Completeness_vec[k]); 
+	  if(PDG_vec[k]==2212 && PID_vec[k]==-1){
+=======
+	  	  
+	  if(ProOrPi_vec[k]==0 && PID_vec[k]==-1){
+>>>>>>> 6f00b7874a1b0801b88832bb262700781aae568e
+	    goodProtons[trueMulti]+=1;
+	  }else if(PDG_vec[k]==211 && PID_vec[k]==-1){
+	    badProtons[trueMulti]+=1;
+	  }else if(PDG_vec[k]==2212 && PID_vec[k]==1){
+	    missedProtons[trueMulti]+=1;
+	  }
+	  
+	  //fill more histograms
+	
+	  totalTracksPassingSingleCut[trueMulti]+=1;
+  
+	  //Check PDG here
+	  
+	  if(PDG_vec[k]==2212){
+	    std::cout << "PROTON!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+	  }else if(PDG_vec[k]==211){
+	    std::cout << "PION!!!!!!!!!!!!!!!!!!!!!!!!!!" << std::endl;
+	  }else{std::cout << PDG_vec[k] << std::endl;}
+
+	}
+      }      
+    }
+  } 
+  
+  
+  for(int i=1; i<21; ++i) {
+    
+    std::cout << "MEAN Cleanliness of multiplicity " << i << " was: " << CleanlinessVsM[i]->GetMean() << std::endl;
+    
+    if(i==1){
+      if (CleanlinessVsM[1]->GetMean()>1.00){
+	MeanCleanliness.SetBinContent(1,1);
+      }
+    }else{
+      MeanCleanliness.SetBinContent(i,CleanlinessVsM[i]->GetMean());
+    }
+    if(i>1){MeanCleanliness.SetBinContent(i,CleanlinessVsM[i]->GetMean());}
+
+    MeanCleanliness.SetBinError(i,CleanlinessVsM[i]->GetRMS());
+    MeanCompleteness.SetBinContent(i, CompletenessVsM[i]->GetMean());
+    MeanCompleteness.SetBinError(i, CompletenessVsM[i]->GetRMS());
+
+    //Fill the 2D scan for every multiplicity
+    for(int cleanbin=1; cleanbin<11; ++cleanbin){
+      for(int compbin=1; compbin<11; ++compbin){
+
+	double goodTracks = (double)totalNumberOfGoodTracks[i][cleanbin-1][compbin-1];	
+	double trackEfficiency = round(goodTracks/(double)totalNumberOfPaths[i]*100)/100;
+	
+	TrackEfficiencyScan[i]->SetBinContent(cleanbin, compbin, trackEfficiency);
+	
+      }
+    }
+       
+    double SingleCutPurity = round((double)totalTracksPassingSingleCut[i]/(double)totalNumberOfPaths[i]*100)/100;
+    double SingleCutEfficiency = round((double)totalTracksPassingSingleCut[i]/(double)totalNumberOfTrueTracks[i]*100)/100;
+    
+    TrackPurity.SetBinContent(i,SingleCutPurity);
+    TrackEfficiency.SetBinContent(i,SingleCutEfficiency);    
+    
+    int numberOfTrueProtons = goodProtons[i]+missedProtons[i];
+    int numberOfRecoProtons = goodProtons[i]+badProtons[i];
+
+
+    std::cout << "Have found " << goodProtons[i] << " good Protons, " << missedProtons[i] << " missed Protons and " << badProtons[i] << " bad Protons." << std::endl; 
+
+    double proEff = (double)goodProtons[i]/(double)numberOfTrueProtons;
+    double proPur = (double)goodProtons[i]/(double)numberOfRecoProtons;
+    
+    ProtonEfficiency.SetBinContent(i,proEff);
+    ProtonPurity.SetBinContent(i,proPur);    
+    
+    std::cout << "Proton Efficiency and Purity was: " << proEff << " and " << proPur << std::endl;
+  }
+
+  
+  dEdx.SetLineColor(kBlue);
+  TrackLength.SetLineColor(kBlue);
+  ChargeSum.SetLineColor(kRed);
+  
+  TFile outf("PID.root", "RECREATE");
+
+
+
+  for(int i=1; i<21; ++i) {
+    TCanvas *C = new TCanvas("C","",750,750);
+    C->cd();
+    gStyle->SetOptStat("");
+    TrackEfficiencyScan[i]->GetXaxis()->SetTitle("Cleanliness Cut");
+    TrackEfficiencyScan[i]->GetYaxis()->SetTitle("Completeness Cut");
+    TrackEfficiencyScan[i]->GetYaxis()->SetTitleOffset(1.5);
+    TrackEfficiencyScan[i]->GetZaxis()->SetRange(0,1);
+    TrackEfficiencyScan[i]->SetOption("COL2 TEXT");
+    TrackEfficiencyScan[i]->Write();
+    //TrackEfficiencyScan[i]->Draw();
+    //char canvas[15];
+    //sprintf(canvas, "Scan%d.png", i);
+    //C->Print(canvas);
+    delete C;
+  }
+
+  
+  TH2D AverageTrackEfficiency("AverageTrackEff", "", cleanbins, 0.5, 1.00, compbins, 0.5, 1.00);
+
+  for(int m=3; m<17; ++m){
+    AverageTrackEfficiency.Add(TrackEfficiencyScan[m]);    
+  }
+
+
+  
+  AverageTrackEfficiency.Scale(1.00/14.00);  
+  TCanvas *C = new TCanvas("C","",750,750);
+  C->cd();
+  gStyle->SetOptStat("");
+  gStyle->SetPaintTextFormat("4.2f");
+  AverageTrackEfficiency.GetXaxis()->SetTitle("Cleanliness Cut");
+  AverageTrackEfficiency.GetYaxis()->SetTitle("Completeness Cut");
+  AverageTrackEfficiency.GetYaxis()->SetTitleOffset(1.5);
+  AverageTrackEfficiency.GetZaxis()->SetRange(0,1);
+  AverageTrackEfficiency.SetOption("COL2 TEXT");
+  AverageTrackEfficiency.Write();
+  AverageTrackEfficiency.Draw();
+  C->Print("AverageTrackEfficiency.png");
+  
+  TrackPurity.GetYaxis()->SetRangeUser(0,1);
+  TrackPurity.SetMarkerStyle(kFullTriangleDown);
+  TrackPurity.Draw("P");
+  C->Print("TrackPurityMinimalCut.png");
+
+  TrackEfficiency.GetYaxis()->SetRangeUser(0,1);
+  TrackEfficiency.SetMarkerStyle(kFullTriangleDown);
+  TrackEfficiency.Draw("P");
+  C->Print("TrackEfficiencyMinimalCut.png");
+
+  ProtonEfficiency.GetYaxis()->SetRangeUser(0,1);
+  ProtonEfficiency.SetMarkerStyle(kFullTriangleDown);
+  ProtonEfficiency.SetMarkerColor(kRed);
+  ProtonEfficiency.Draw("P");
+  C->Print("ProtonEfficiency.png");
+
+  ProtonPurity.GetYaxis()->SetRangeUser(0,1);
+  ProtonPurity.SetMarkerStyle(kFullTriangleDown);
+  ProtonPurity.SetMarkerColor(kRed); 
+  ProtonPurity.Draw("P");
+  C->Print("ProtonPurity.png");
+
+  MeanCleanliness.GetYaxis()->SetRangeUser(0,1);
+  MeanCleanliness.SetMarkerStyle(kFullTriangleDown);
+  MeanCleanliness.Draw("P");
+  C->Print("MeanCleanliness.png");
+  
+  MeanCompleteness.GetYaxis()->SetRangeUser(0,1);
+  MeanCompleteness.SetMarkerStyle(kFullTriangleDown);
+  MeanCompleteness.Draw("P");
+  C->Print("MeanCompleteness.png");
+
+  Multiplicities.Draw("COL2");
+  C->Print("Multiplicities.png");
+
+  dEdx.Write();
+  TrackLength.Write();
+  ChargeSum.Write();
+  Cleanliness.Write();
+  Completeness.Write();
+  MeanCleanliness.Write();
+  MeanCompleteness.Write();
+  TrackEfficiency.Write();
+  TrackPurity.Write();
+  ProtonEfficiency.Write();
+  ProtonPurity.Write();
+  Multiplicities.Write();
+  //write more histograms
+
+
+}
+
